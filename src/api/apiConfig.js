f26f14889d1a5b7678c3e4f23082b32d3cc7c187
JatinAyoo <jatin9898yadav@gmail.com>
2024-05-29 09:17:24 +0530

    API key updated

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '2400f395345d8a212e51a9f0fee9f3fb',
+    apiKey: '9cab5cbdb3aaa8098c29ca67ec29e666',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
